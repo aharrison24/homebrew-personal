41254d3ebb3c8c85720096e9427f64c01cf3dba5
Alastair Harrison <alastair@oxbotica.com>
2016-03-22 17:06:15 +0000

    Add git-prebase formula

---
+++ git-prebase.rb
@@ -0,0 +1,8 @@
+class GitPrebase < Formula
+  homepage "https://github.com/koreno/prebase"
+  head "https://github.com/koreno/prebase.git"
+
+  def install
+    bin.install 'git-prebase'
+  end
+end
