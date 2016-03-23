86c2161f758fedc5648bb9d22489caf40f11664d
Alastair Harrison <aharrison24@gmail.com>
2016-03-23 08:48:34 +0000

    Add 'stable' commit for git-prebase

--- git-prebase.rb
+++ git-prebase.rb
@@ -1,5 +1,8 @@
 class GitPrebase < Formula
   homepage "https://github.com/koreno/prebase"
+  url "https://github.com/koreno/prebase.git", :revision => "20fc8f331bf14427f964f6273274e36627583d52"
+  version "0.0.1-20fc8f3"
+
   head "https://github.com/koreno/prebase.git"
 
   def install
